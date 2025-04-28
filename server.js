ae2e9657b216f89b9d47fd3c56f06474c6faddee
cutterя <sofagucci228@gmail.com>
2025-04-28 12:50:02 +0500

    index

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = 80;
-
-app.use(express.static('pizza-app/dist'));
-
-app.listen(port, () => console.log("server has been started"));
